91f622ef192874e69bb02536ae2ca2383f44604d
Marvin Kokor <kokor@gmx.de>
2022-04-09 18:30:44 +0200

    init

---
+++ src/main/java/de/udg/socialwall/SocialwallApplication.java
@@ -0,0 +1,13 @@
+package de.udg.socialwall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialwallApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocialwallApplication.class, args);
+	}
+
+}
